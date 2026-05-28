11db344208f21a15002496e93ed92b894ac12eb1
Rozioi <gtavvedeos@gmail.com>
2026-05-28 23:04:59 +0300

    DT-92 feature: implement recordings history sidebar and delete logic

---
+++ src/app/routes/router.tsx
@@ -0,0 +1,73 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+
+import { LandingPage } from '@/pages/LandingPage/LandingPage';
+import { AppLayout } from '@/pages/layouts/AppLayout';
+import { TreePage } from '@/pages/TreePage';
+import { CurrentLecturePage } from '@/pages/CurrentLecturePage';
+import SettingsPage from '@/pages/settings/SettingsPage';
+import { HomeworksPage } from '@/pages/HomeworksPage/HomeworksPage';
+import { DebugPage } from '@/pages/debug/DebugPage';
+import { ChainsPage } from '@/pages/chains/ChainsPage';
+import { TestsPage } from '@/pages/tests/TestsPage';
+import PitchDetectorPiano from '@/features/Pitcher/Pitcher';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+  {
+    path: '/app',
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="tree" replace />,
+      },
+      {
+        path: 'tree',
+        element: <TreePage />,
+      },
+      {
+        path: 'current/lecture',
+        element: <CurrentLecturePage />,
+      },
+      {
+        path: 'settings',
+        element: <SettingsPage />,
+      },
+      {
+        path: 'mic',
+        element: <PitchDetectorPiano />,
+      },
+      {
+        path: 'homeworks',
+        element: <HomeworksPage />,
+      },
+      {
+        path: 'homeworks/:homeworkId?',
+        element: <HomeworksPage />,
+      },
+      {
+        path: 'chains',
+        element: <ChainsPage />,
+      },
+      {
+        path: 'chains/:chainId?',
+        element: <ChainsPage />,
+      },
+      {
+        path: 'tests',
+        element: <TestsPage />,
+      },
+      {
+        path: 'tests/:testId?',
+        element: <TestsPage />,
+      },
+      {
+        path: 'debug',
+        element: <DebugPage />,
+      },
+    ],
+  },
+]);
